a71af9925e5f61d5a5defc0a2cbb8792fe8978ad
mrndolo <callistomutunga@gmail.com>
2024-06-13 08:57:16 +0300

    Added toast animation and progress bars

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function showToast(msg){
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
 
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
     setTimeout(()=>{
         toast.remove()
     },5000);
